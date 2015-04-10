d0714f229807e0ece7b7c47f4c78d4366bc175e5
Aviv Shafir <aviv@optimalfinance.co.il>
2015-04-10 17:34:23 +0300

    finished section 4

--- KnockoutSPADocs/Scripts/main.js
+++ KnockoutSPADocs/Scripts/main.js
@@ -4,5 +4,15 @@ $(function () {
     viewModel.addItem(new Document("hello", "world"));
     viewModel.addItem(new Document("goodbye", "world"));
 
+    //debugging an observable exapmle (watching for a value change on the selectedItem property)
+    viewModel.selectedItem.subscribe(function(newValue) {
+        debugger;
+    });
+
+    //override the default equality comparer of knockout
+    ko.observable.fn.equalityComparer = function(a ,b) {
+        return a === b;
+    };
+
     ko.applyBindings(viewModel);
 });
